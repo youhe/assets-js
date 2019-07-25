d38de8b3b1f84133cbeee7f119819a1d7a7481ef
youhe <uta.syarin@gmail.com>
2019-07-25 17:31:57 +0900

    pass

--- gulp/tasks/replace.js
+++ gulp/tasks/replace.js
@@ -6,9 +6,9 @@ const conf = require('../conf').replace;
 
 gulp.task('replaceHtml', () => {
   const regJs = new RegExp(`(src=")(${DIR.PATH}\/js\/)([a-z0-9_\.\-]*)(\.js")`);
-  const regCss = new RegExp(`(href="${DIR.PATH}\/css\/)([a-z0-9_\.\-]*)(\.css")`);
+  const regCss = new RegExp(`(href=")(${DIR.PATH}\/css\/)([a-z0-9_\.\-]*)(\.css")`);
   return gulp.src(conf.html.src)
-    .pipe($.replace(regJs, '$1/build$2$3.min$4'))
-    .pipe($.replace(regCss, '$1$2.min$3'))
+    .pipe($.replace(regJs, '$1/assets-js/build$2$3.min$4'))
+    .pipe($.replace(regCss, '$1/assets-js/build$2$3.min$4'))
     .pipe(gulp.dest(conf.html.dest));
 });
