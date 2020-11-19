ed1e190b1cfb401cf7399b94f0e4bddccb683e46
youhe <uta.syarin@gmail.com>
2020-11-19 13:07:22 +0900

    time追加

--- src/js/modules/common/page-scroll.js
+++ src/js/modules/common/page-scroll.js
@@ -1,16 +1,27 @@
 const easeing = require("./easing");
 
-export default function(start, end) {
-  scrollAnimation(0, start, end);
-
-  function scrollAnimation(p, sy, ey) {
+/**
+ * 
+ * @param {number} start 
+ * @param {number} end 
+ * @param {number} time 
+ * 
+ * time
+ * 0.016で1秒
+ * 0.012で1.5秒
+ * 0.008で2秒
+ */
+export default function(start, end, time = 0.012) {
+  function scrollAnimation(p, sy, ey, time) {
     p = Math.min(p, 1.0);
     if (p < 1)
       requestAnimationFrame(() => {
-        scrollAnimation(p + 0.02, sy, ey);
+        scrollAnimation(p + time, sy, ey, time);
       });
 
     const tP = easeing.easeInOutQuad(p);
     scrollTo(0, (ey - sy) * tP + sy);
   }
+
+  scrollAnimation(0, start, end, time);
 }
